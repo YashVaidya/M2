de151550a2ee5027eed640969f93331ce4b9e84b
YashVaidya <yashvsuperstar@gmail.com>
2018-09-05 21:09:24 -0400

    werwe

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,15 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String c = "";
+      for(int i = 0;i<input.length();i++) {
+        char k  = input.charAt(i);
+        k--;
+        c = c + k;
+
+      }
+      return c;
+    }
     }
     
     /**
